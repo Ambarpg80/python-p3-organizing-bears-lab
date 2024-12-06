1b35bf0d15f26293695c0bbce42329c2e9801a6d
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2024-12-06 15:38:04 -0500

    created bears table, added bears and added queries in requested files.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 12, "M", "brown", "mischievous", 0),
+(2,"Rowdy", 4, "M", "blue", "playful", 1),
+(3,"Tabitha", 9, "F", "black", "nice", 1),
+(4,"Sergeant Brown", 30, "M", "brown", "leader", 0),
+(5,"Melissa", 13, "F", "black", "happy", 1),
+(6,"Grinch", 25, "M", "blonde", "grumpy", 1),
+(7,"Wendy", 3, "F", "cinnamon", "playful", 1),
+(8,null, 16, "M", "blue", "grumpy", 0);
